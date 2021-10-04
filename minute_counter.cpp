d5ea5d36fdb716d2db5c4f401ed6bd2b1870bab2
AAryff <76688919+AAryff@users.noreply.github.com>
2021-10-04 11:59:39 +0800

    Update on 4/10/2021
    Added a feature where if user enters end time smaller than start time, program notifies user start time is bigger than end time and asks user for a new set of start time and end time

--- minute_counter.cpp
+++ minute_counter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 class period {
@@ -13,6 +14,7 @@ class period {
         int total_minutes;
 };
 
+//function to display the time based on data given by the user
 void display_time(int hours, int minutes, string halve){
     if (minutes < 10){
         cout << hours << ":" << "0" << minutes << " " << halve << endl;
@@ -21,18 +23,21 @@ void display_time(int hours, int minutes, string halve){
     }
 }
 
+
+//function to count total minutes in a time
 int count_minutes(int hours, int minutes, string halve){
     int total;
     if (halve == "PM" && hours != 12){
         total = 12 * 60 + hours * 60 + minutes;
     } else if (hours == 12 && halve =="AM"){ //12:00 AM means total minutes is 0 instead of 720 
         total = minutes;
-    } else {
+    } else{
         total = hours * 60 + minutes;
     }
     return total;
 }
 
+//function to get hour data from the user
 int period::get_hour(){
     do {
     cout << "Hour (1 - 12): ";
@@ -46,6 +51,7 @@ int period::get_hour(){
     return hour;
 }
 
+//function to receive minute data from user
 int period::get_minute(){
     do {
     cout << "Minute (1 - 59): ";
@@ -59,6 +65,7 @@ int period::get_minute(){
     return minute;
 }
 
+//function to receive halve data from the user
 string period::get_halve(){
 
     do {
@@ -70,24 +77,38 @@ string period::get_halve(){
     return halve;
 }
 
+//display the duration in (1-12) Hours and (1-59) minutes format
+void display_duration_time(int duration_total_minutes){
+    int duration_hour = duration_total_minutes/60;
+    double duration_minute = remainder(duration_total_minutes, 60);
+    cout << "Duration: " << duration_hour << " Hours " << duration_minute << " Minutes" << endl;
+}
+
 int main(){
     period starting; 
     period ending;
 
-    cout << "Enter Start time: ";
-    starting.hour = starting.get_hour();
-    starting.minute = starting.get_minute();
-    starting.halve = starting.get_halve();
-    cout << "Start: "; display_time(starting.hour, starting.minute, starting.halve);
-    starting.total_minutes = count_minutes(starting.hour, starting.minute, starting.halve);
-    
-    cout << "Enter End time: ";
-    ending.hour = ending.get_hour();
-    ending.minute = ending.get_minute();
-    ending.halve = ending.get_halve();
-    cout << "End: "; display_time(ending.hour, ending.minute, ending.halve);
-    ending.total_minutes = count_minutes(ending.hour, ending.minute, ending.halve);
+    do{
+        cout << "Enter Start time: ";
+        starting.hour = starting.get_hour();
+        starting.minute = starting.get_minute();
+        starting.halve = starting.get_halve();
+        cout << "Start: "; display_time(starting.hour, starting.minute, starting.halve); //display start time given by the user
+        starting.total_minutes = count_minutes(starting.hour, starting.minute, starting.halve);
 
-    int duration = ending.total_minutes - starting.total_minutes;
-    cout << "Duration: " << duration << " minutes";
+        cout << "Enter End time: ";
+        ending.hour = ending.get_hour();
+        ending.minute = ending.get_minute();
+        ending.halve = ending.get_halve();
+        cout << "End: "; display_time(ending.hour, ending.minute, ending.halve); //display end time given by the user
+        ending.total_minutes = count_minutes(ending.hour, ending.minute, ending.halve);
+
+        if (starting.total_minutes > ending.total_minutes){
+            cout << "Starting time is bigger than end time, choose a new time" << endl;
+        } else {
+            int duration_total_minutes = ending.total_minutes - starting.total_minutes;
+            display_duration_time(duration_total_minutes);
+        }
+        
+    } while (starting.total_minutes > ending.total_minutes);
 }
